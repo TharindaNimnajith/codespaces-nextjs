2348df3597c604c90c72599bf42d913b35c7dfec
Tharinda Nimnajith Rajapaksha <52395884+TharindaNimnajith@users.noreply.github.com>
2022-12-12 16:51:53 +0000

    init

--- components/ClickCount.js
+++ components/ClickCount.js
@@ -3,6 +3,7 @@ import Button from './Button'
 
 export default function ClickCount() {
   const [count, setCount] = useState(0)
+
   const increment = useCallback(() => {
     setCount((v) => v + 1)
   }, [setCount])
